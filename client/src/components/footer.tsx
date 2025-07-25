4edb4752d93c5508c981e51456fcb3577deb0021
Ian Dela Cruz <iandlcr20@gmail.com>
2025-07-25 19:10:51 -0400

    changing hours of operation

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -99,7 +99,7 @@ export default function Footer() {
                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd"></path>
                 </svg>
-                Mon-Fri: 9:00 AM - 5:00 PM
+                Mon-Sunday: Flexible
               </p>
               <div className="flex space-x-4 mt-4">
                 <a 
